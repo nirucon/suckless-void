e66ac3a057346c85ddff1d6f7ab04ecba51a3d38
Nicklas Rudolfsson <66663564+nirucon@users.noreply.github.com>
2023-01-14 19:11:24 +0100

    Add files via upload

---
+++ symbolic-links.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# some "automation" for my suckless setup
+
+echo "create symbolic links..."
+ln -s /media/1TB-TWO/Music ~/Music/mp3 &
+ln -s /media/1TB-ONE/Nextcloud/Wallpapers ~/Pictures/Wallpapers &
+ln -s /media/1TB-ONE/Nextcloud/Screenshots ~/Pictures/Screenshots &
+ln -s /media/1TB-ONE/Nextcloud/ ~/Nextcloud &
+ln -s /media/1TB-ONE/Nextcloud/Tech/Scripts ~/Scripts &
+
+sleep 1
+
+echo "links to mp3, Wallpaers, Screenshots, Nextcloud, Scripts = done!"
