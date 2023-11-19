95c955d8426e2e96497010cc0307304b58b847ed
Nicklas Rudolfsson <66663564+nirucon@users.noreply.github.com>
2023-11-19 08:45:39 +0100

    added: maim, xclip

--- void-suckless-base.sh
+++ void-suckless-base.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo xbps-install base-devel libX11-devel libXft-devel libXinerama-devel freetype-devel fontconfig-devel xorg nitrogen picom alsa-utils imlib2-devel libXrandr-devel nerd-fonts-ttf xautolock scrot
+sudo xbps-install base-devel libX11-devel libXft-devel libXinerama-devel freetype-devel fontconfig-devel xorg nitrogen picom alsa-utils imlib2-devel libXrandr-devel nerd-fonts-ttf xautolock scrot xclip maim
